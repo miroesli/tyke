7506135204090db3f41b0ab1afba356a897c9464
miroesli <misha.roesli@gmail.com>
2019-11-28 22:17:35 -0800

    feat: update generated javascript

--- dist/handlers.js
+++ dist/handlers.js
@@ -1,51 +1,56 @@
+"use strict";
 /**
  * Don't worry about anything in this file,
  * focus on writing your snake logic in index.js endpoints.
  */
-var poweredByHandler = function (req, res, next) {
-    res.setHeader('X-Powered-By', 'Battlesnake');
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.poweredByHandler = function (req, res, next) {
+    res.setHeader("X-Powered-By", "Battlesnake");
     next();
 };
-var fallbackHandler = function (req, res, next) {
+exports.fallbackHandler = function (req, res, next) {
     console.dir(req.baseUrl);
     // Root URL path
-    if (req.baseUrl === '') {
+    if (req.baseUrl === "") {
         res.status(200);
         return res.send("\n      Battlesnake documentation can be found at\n       <a href=\"https://docs.battlesnake.io\">https://docs.battlesnake.io</a>.\n    ");
     }
     // Short-circuit favicon requests
-    if (req.baseUrl === '/favicon.ico') {
-        res.set({ 'Content-Type': 'image/x-icon' });
+    if (req.baseUrl === "/favicon.ico") {
+        res.set({ "Content-Type": "image/x-icon" });
         res.status(200);
         res.end();
         return next();
     }
     // Reroute all 404 routes to the 404 handler
-    var err = new Error();
-    err.status = 404;
+    // const err = new Error();
+    // err.status = 404;
+    var err = new Error("Not Found");
+    res.status(404);
     return next(err);
 };
-var notFoundHandler = function (err, req, res, next) {
+exports.notFoundHandler = function (err, req, res, next) {
     if (err.status !== 404) {
         return next(err);
     }
     res.status(404);
     return res.send({
         status: 404,
-        error: err.message || "These are not the snakes you're looking for",
+        error: err.message || "These are not the snakes you're looking for"
     });
 };
-var genericErrorHandler = function (err, req, res, next) {
+exports.genericErrorHandler = function (err, req, res, next) {
     var statusCode = err.status || 500;
     res.status(statusCode);
     return res.send({
         status: statusCode,
-        error: err,
+        error: err
     });
 };
 module.exports = {
-    fallbackHandler: fallbackHandler,
-    notFoundHandler: notFoundHandler,
-    genericErrorHandler: genericErrorHandler,
-    poweredByHandler: poweredByHandler,
+    fallbackHandler: exports.fallbackHandler,
+    notFoundHandler: exports.notFoundHandler,
+    genericErrorHandler: exports.genericErrorHandler,
+    poweredByHandler: exports.poweredByHandler
 };
+//# sourceMappingURL=handlers.js.map
--- dist/index.js
+++ dist/index.js
@@ -1,46 +1,49 @@
-var bodyParser = require('body-parser');
-var express = require('express');
-var logger = require('morgan');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var bodyParser = require("body-parser");
+var express = require("express");
+var logger = require("morgan");
 var app = express();
-var _a = require('./handlers.js'), fallbackHandler = _a.fallbackHandler, notFoundHandler = _a.notFoundHandler, genericErrorHandler = _a.genericErrorHandler, poweredByHandler = _a.poweredByHandler;
+var handlers_1 = require("./handlers");
 // For deployment to Heroku, the port needs to be set using ENV, so
 // we check for the port number in process.env
-app.set('port', (process.env.PORT || 9001));
-app.enable('verbose errors');
-app.use(logger('dev'));
+app.set("port", process.env.PORT || 9001);
+app.enable("verbose errors");
+app.use(logger("dev"));
 app.use(bodyParser.json());
-app.use(poweredByHandler);
+app.use(handlers_1.poweredByHandler);
 // --- SNAKE LOGIC GOES BELOW THIS LINE ---
 // Handle POST request to '/start'
-app.post('/start', function (request, response) {
+app.post("/start", function (request, response) {
     // NOTE: Do something here to start the game
     // Response data
     var data = {
-        color: '#DFFF00',
+        color: "#DFFF00"
     };
     return response.json(data);
 });
 // Handle POST request to '/move'
-app.post('/move', function (request, response) {
+app.post("/move", function (request, response) {
     // NOTE: Do something here to generate your move
     // Response data
     var data = {
-        move: 'up',
+        move: "up" // one of: ['up','down','left','right']
     };
     return response.json(data);
 });
-app.post('/end', function (request, response) {
+app.post("/end", function (request, response) {
     // NOTE: Any cleanup when a game is complete.
     return response.json({});
 });
-app.post('/ping', function (request, response) {
+app.post("/ping", function (request, response) {
     // Used for checking if this snake is still alive.
     return response.json({});
 });
 // --- SNAKE LOGIC GOES ABOVE THIS LINE ---
-app.use('*', fallbackHandler);
-app.use(notFoundHandler);
-app.use(genericErrorHandler);
-app.listen(app.get('port'), function () {
-    console.log('Server listening on port %s', app.get('port'));
+app.use("*", handlers_1.fallbackHandler);
+app.use(handlers_1.notFoundHandler);
+app.use(handlers_1.genericErrorHandler);
+app.listen(app.get("port"), function () {
+    console.log("Server listening on port %s", app.get("port"));
 });
+//# sourceMappingURL=index.js.map
